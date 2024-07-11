45abbaf213484b52e931d3a90b85dd2915b42523
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-07-10 21:11:00 -0300

    chore: initial nav and mode styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
-const App = () => {
+function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
