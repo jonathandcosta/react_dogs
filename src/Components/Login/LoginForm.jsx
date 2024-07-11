8f3c91b4caad5a83eb9f58ad1a708d197120305b
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-07-11 09:06:37 -0300

    chore: initial estruture page Login and Rotes

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
 };
 
 export default LoginForm;
