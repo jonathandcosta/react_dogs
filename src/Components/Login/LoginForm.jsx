c6e00da11a01b6e4f8db472e061976095a14661e
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-07-15 14:49:12 -0300

    chore: add login api

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { TOKEN_POST } from '../../api';
 
 const LoginForm = () => {
   const username = useForm('email');
@@ -12,6 +13,22 @@ const LoginForm = () => {
   // consumo da API
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (username.validate && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      fetch(url, options)
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
