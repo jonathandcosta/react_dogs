5dfb128b93ff851af85b3c79792de508bf46704b
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-07-30 15:43:47 -0300

    chore: the initial page create login

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Email" type="email" name="email" />
+        <Input label="Senha" type="passaword" name="password" />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
