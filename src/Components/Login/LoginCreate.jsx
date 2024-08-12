d33675732ccf58a85939acfc9ac6e3bb80e0c8b8
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-08-12 14:35:29 -0300

    chore: validation user - page register

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -5,10 +5,10 @@ import useForm from '../../Hooks/useForm';
 
 const LoginCreate = () => {
   // validar as informações do usuário no input.
-  //obs: para forçar o preenchimento do input na page usa-se a const dessa forma-> const username = useForm('nome') com o parametro que será exigido preenchido na const.
+  //obs: para forçar o preenchimento do input com a validação do regex na page usa-se a const dessa forma-> const username = useForm('nome') com o parametro que será exigido preenchido na const.
   const username = useForm();
   const email = useForm('email');
-  const password = useForm();
+  const password = useForm('password');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -18,9 +18,9 @@ const LoginCreate = () => {
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Email" type="email" name="email" />
-        <Input label="Senha" type="passaword" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Cadastrar</Button>
       </form>
     </section>
