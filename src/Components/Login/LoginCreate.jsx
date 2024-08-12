5546e5643a61dc178bb815ad206204f016128c51
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-08-12 16:02:43 -0300

    chore: user register with API validation

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
 
 const LoginCreate = () => {
   // validar as informações do usuário no input.
   //obs: para forçar o preenchimento do input com a validação do regex na page usa-se a const dessa forma-> const username = useForm('nome') com o parametro que será exigido preenchido na const.
   const username = useForm();
   const email = useForm('email');
-  const password = useForm('password');
+  const password = useForm(); //existe um regex pronto no arquivo "useform"
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    console.log(response);
   }
 
   return (
