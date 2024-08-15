a117a28d456cfca1642360eea5e3c559c2005fe5
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-08-14 21:28:38 -0300

    chore: Routes user feed

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
 
 const User = () => {
-  return <div>Usuário</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
