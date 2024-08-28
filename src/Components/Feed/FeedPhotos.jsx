9104bd63a707e31377357bd9166f592860137649
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-08-27 21:58:18 -0300

    chore: show images of users

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../api';
+import Error from '../Helper/Error';
 
 const FeedPhotos = () => {
+  //puxa as imagens do usuário
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { response, json } = await request(url, options);
+      console.log(json);
+    }
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
   return (
     <div>
       <FeedPhotosItem />
