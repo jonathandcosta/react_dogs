7732d3c1d9066d21c0952ca758c6d4d358029842
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-08-30 14:21:00 -0300

    chore: finaly modal + styles

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './FeedModal.module.css';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
 const FeedModal = ({ photo }) => {
   const { data, error, loading, request } = useFetch();
@@ -11,7 +14,13 @@ const FeedModal = ({ photo }) => {
     request(url, options);
   }, [photo, request]);
 
-  return <div className={styles.modal}></div>;
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
