f3fa15006e47aa5ef89d103e0fa6a0e831b47ae2
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-09-26 16:39:51 -0300

    chore: comment (bug)

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Enviar from '../../Assets/enviar.svg?react';
+import useFetch from '../../Hooks/useFetch';
+import { COMMENT_POST } from '../../api';
 
-const PhotoCommentsForm = () => {
-  return <div></div>;
+const PhotoCommentsForm = ({ id }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    await request(url, options);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+    </form>
+  );
 };
 
 export default PhotoCommentsForm;
