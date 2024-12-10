3d7087a2876a4d0161b5c36ed0b4c74d096bb171
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-12-10 15:50:55 -0300

    chore: style comments photo

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import Enviar from '../../Assets/enviar.svg?react';
 import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import { COMMENT_POST } from '../../api';
 
-const PhotoCommentsForm = ({ id }) => {
+import styles from './PhotoCommentsForm.module.css';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
   const { request, error } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    await request(url, options);
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };
