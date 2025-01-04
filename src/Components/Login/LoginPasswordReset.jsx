76c47a29b626e8715bdd487ad31fd91c27130e2d
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-01-03 22:21:54 -0300

    chore: details

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -38,7 +38,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeleft">
       <Head title="Resete a senha" />
       <h1 className="title">Resete sua senha</h1>
       <form onSubmit={handleSubmit}>
@@ -55,7 +55,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
